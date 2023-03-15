a5be66f33dc41481aa30b1462db8d9db8d72137d
SiliņšL <SiliL@gmail.com>
2023-03-15 09:13:00 +0200

    Pabeigta picas izveide, gan ja pasūta uz adresi, gan ja brauc pats pakaļ

--- src/picerija/Picerija.java
+++ src/picerija/Picerija.java
@@ -22,48 +22,88 @@ public class Picerija {
 		
 		
 		do{
-			izvele = (String)JOptionPane.showInputDialog(null,"Izvelies darbibu" , "Darbibas izvele",JOptionPane.QUESTION_MESSAGE,null,opcijas,opcijas[2]);
+		izvele = (String)JOptionPane.showInputDialog(null,"Izvelies darbibu" , "Darbibas izvele",JOptionPane.QUESTION_MESSAGE,null,opcijas,opcijas[2]);
 			
-			switch(izvele){
+		switch(izvele){
 			
 			
-			case"Sakt darbu":
+		case"Sakt darbu":
 				
-				JOptionPane.showMessageDialog(null, "Jums zvana klients", "Informācija",JOptionPane.INFORMATION_MESSAGE);
+			JOptionPane.showMessageDialog(null, "Jums zvana klients", "Informācija",JOptionPane.INFORMATION_MESSAGE);
 				
-				do{
-					telNr = JOptionPane.showInputDialog("Klienta nummurs... ");
+			do{
+				telNr = JOptionPane.showInputDialog("Klienta nummurs... ");
  
-				}while (!(telNr.startsWith("2") && telNr.length() == 8));
+			}while (!(telNr.startsWith("2") && telNr.length() == 8));
 				
-				vards = JOptionPane.showInputDialog("Klientu sauc...");
+			vards = JOptionPane.showInputDialog("Klientu sauc...");
 				
-				izvele = (String)JOptionPane.showInputDialog(null, "Vai jums bus japiegada pica, vai paši atbrauksiet pakaļ? ",
+			izvele = (String)JOptionPane.showInputDialog(null, "Vai jums bus japiegada pica, vai paši atbrauksiet pakaļ? ",
 						"Par piegādi", JOptionPane.QUESTION_MESSAGE, null, atbilde, atbilde[0]);
-				izveletaisIndekss = Arrays.asList(atbilde).indexOf(izvele);
-      			boolean pakal = (izveletaisIndekss==0) ? false : true;
-      			if(pakal==false){
+			izveletaisIndekss = Arrays.asList(atbilde).indexOf(izvele);
+      		boolean pakal = (izveletaisIndekss==0) ? false : true;
+      		if(pakal==false){
       				
-      				String[] pilsetas = {"Liepaja", "Grobiņa", "Cimdinieki"};
-      				String pilIzvele;
-      				pilIzvele = (String)JOptionPane.showInputDialog(null,"Izvelies pilsētu, uz kuru gribi lai piegādā picu!" , "Pilsētas izvēle",JOptionPane.QUESTION_MESSAGE,null,pilsetas,pilsetas[2]);
+      			String[] pilsetas = {"Liepaja", "Grobiņa", "Cimdinieki"};
+      			String pilIzvele;
+      			pilIzvele = (String)JOptionPane.showInputDialog(null,"Izvelies pilsētu, uz kuru gribi lai piegādā picu!" ,
+      					"Pilsētas izvēle",JOptionPane.QUESTION_MESSAGE,null,pilsetas,pilsetas[2]);
       				
-      				adrese = JOptionPane.showInputDialog("Labi, kāda ir jūsu adrese?");
+      			adrese = JOptionPane.showInputDialog("Labi, kāda ir jūsu adrese?");
       				
-      				JOptionPane.showMessageDialog(null, "Tātad, veiksim piegādi uz "+pilIzvele+" "+adrese+" ielas", "Informācija",JOptionPane.INFORMATION_MESSAGE);
-      			}else{
+      			JOptionPane.showMessageDialog(null, "Tātad, veiksim piegādi uz "+pilIzvele+" "+adrese+" ielas",
+      					"Informācija",JOptionPane.INFORMATION_MESSAGE);
+      			
+      			
+      			String[] picas = {"Siera picu", "Veģitāro picu", "Peperoni picu","Havaiešu picu","Šķiņķa picu"};
+      			String picIzvele;
+      			picIzvele = (String)JOptionPane.showInputDialog(null,"Kādu picu jūs sūtīsiet?" ,
+      					"Picas izvēle",JOptionPane.QUESTION_MESSAGE,null,picas,picas[4]);
+      			
+      			
+      			String[] picuLiel = {"20cm", "25cm", "30cm","35cm","40cm","45cm","50cm"};
+      			String picLIzvele;
+      			picLIzvele = (String)JOptionPane.showInputDialog(null,"Cik liela būs jūsu pica?" ,
+      					"Picas lieluma izvēle",JOptionPane.QUESTION_MESSAGE,null,picuLiel,picuLiel[6]);
+      			
+      			
+      			String[] extra = {"Siers", "Gaļa", "Peperoni","Sīpoli","Mērce","Sēnes","Zaļumi"};
+      			String exIzv;
+      			exIzv = (String)JOptionPane.showInputDialog(null,"Kādas peidevas jūs gribēsiet?" ,
+      					"Picas piedevu izvēle",JOptionPane.QUESTION_MESSAGE,null,extra,extra[6]);
+      			
+      			
+      			String[] daudzumss = {"1", "2", "3","4","5","6","7","8","9","10"};
+      			String daudz;
+      			daudz = (String)JOptionPane.showInputDialog(null,"Cik picas jūs gribēsiet?" ,
+      					"Picas daudzuma izvēle",JOptionPane.QUESTION_MESSAGE,null,daudzumss,daudzumss[9]);
+      		}else{
       				
+      			String[] picas = {"Siera picu", "Veģitāro picu", "Peperoni picu","Havaiešu picu","Šķiņķa picu"};
+      			String picIzvele;
+      			picIzvele = (String)JOptionPane.showInputDialog(null,"Kādu picu jūs sūtīsiet?" ,
+      					"Picas izvēle",JOptionPane.QUESTION_MESSAGE,null,picas,picas[4]);
+      			
+      			
+      			String[] picuLiel = {"20cm", "25cm", "30cm","35cm","40cm","45cm","50cm"};
+      			String picLIzvele;
+      			picLIzvele = (String)JOptionPane.showInputDialog(null,"Cik liela būs jūsu pica?" ,
+      					"Picas lieluma izvēle",JOptionPane.QUESTION_MESSAGE,null,picuLiel,picuLiel[6]);
+      			
+      			
+      			String[] extra = {"Siers", "Gaļa", "Peperoni","Sīpoli","Mērce","Sēnes","Zaļumi"};
+      			String exIzv;
+      			exIzv = (String)JOptionPane.showInputDialog(null,"Kādas peidevas jūs gribēsiet?" ,
+      					"Picas piedevu izvēle",JOptionPane.QUESTION_MESSAGE,null,extra,extra[6]);
+      			
+      			
+      			String[] daudzumss = {"1", "2", "3","4","5","6","7","8","9","10"};
+      			String daudz;
+      			daudz = (String)JOptionPane.showInputDialog(null,"Cik picas jūs gribēsiet?" ,
+      					"Picas daudzuma izvēle",JOptionPane.QUESTION_MESSAGE,null,daudzumss,daudzumss[9]);
       				
-      				
-      			}
-				
-				
-				
-				
-				
-				
-				
-				
+      		}
+
 				break;
 			
 				
